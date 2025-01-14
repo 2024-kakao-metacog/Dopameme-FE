4677e7c4630dc50b6aa5a9caefee226e4d0f0f1e
SANGHYEOP <127101419+ohsanghyeop@users.noreply.github.com>
2025-01-14 13:44:27 +0900

    feat: 메인화면 버튼 컴포넌트 제작 및 메인화면 markup 진행중
    Player.tsx: 볼륨조절 콘솔로그 삭제
    VideoBotton.tsx: 메인화면 버튼 컴포넌트 제작
    main.tsx: 메인화면 markup 진행중
    shorts.tsx: url 안보이도록 삭제
    tailwind.config.js: 메인화면 배경색 커스텀 색상추가

--- src/pages/shorts.tsx
+++ src/pages/shorts.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Player from '../components/Player';
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen">
+    <div className="flex flex-col items-center justify-center h-screen w-screen ">
       {/* Tailwind로 비율 유지 및 최대 크기 설정 */}
-      <div className="relative w-[min(30vw,36rem)] aspect-[294/510]">
+      <div className="relative">
         <div className="absolute bottom-6 left-1 text-white px-4 py-2 rounded-lg z-10" style={{ fontSize: '1.5vw' }}>
-          쇼미더머니 씹어먹는 썬라이즈
+          이것은 제목입니다
         </div>
-        <Player
-          src="http://localhost:8080/hObAvaxlQ7o.mp4-manifest.mpd" //"http://192.168.1.110:3001/v1/videostream/video?manifest=hObAvaxlQ7o.mp4"
-          autoplay={false}
-        />
+        <Player src="여기는 URL 자리" autoplay={false} />
       </div>
     </div>
   );
