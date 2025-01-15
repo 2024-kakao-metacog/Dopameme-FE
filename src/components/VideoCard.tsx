e9257a247902a0d2a6cfac486038b08e7cbd14c1
SANGHYEOP <127101419+ohsanghyeop@users.noreply.github.com>
2025-01-15 22:27:19 +0900

    feat: VideoCard & main page
    main.tsx: 메인화면에 VideoCard 컴포넌트를 표시
    VideoCard.tsx: VideoBotton 과 title 을 묶어 컨테이너로 생성

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import VideoBotton from '../components/VideoBotton';
+
+interface VideoCard {
+  videoId: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  title: string;
+}
+
+interface VideoCardProps {
+  video: VideoCard | null; // 비디오가 없을 수도 있으므로 null 허용
+}
+
+function VideoCard({ video }: VideoCardProps) {
+  return (
+    <div className="flex h-full w-1/4 items-center justify-center px-2">
+      <div className="flex h-auto w-full flex-col space-y-2 ">
+        {/* 비디오 버튼 */}
+        <div className="flex aspect-[9/16] h-auto w-full overflow-hidden rounded-xl ">
+          <Link key={video?.videoId || 'placeholder'} to={video ? '/shorts' : '#'}>
+            {video ? <VideoBotton videoUrl={video.videoUrl} thumbnailUrl={video.thumbnailUrl} /> : <span className="bg-gray-500 text-white">No Video</span>}
+          </Link>
+        </div>
+
+        {/* 제목 */}
+        <div className="flex h-auto w-full text-sm text-white md:text-lg lg:text-xl">
+          <span className="overflow-hidden truncate">{video?.title || 'Empty Slot'}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoCard;
