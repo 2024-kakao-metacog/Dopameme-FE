36351a3dc3f6ee4ef6cfa1afab48aafebc70661e
comgog <102729927+comgog@users.noreply.github.com>
2025-01-15 17:34:05 +0900

    feat: login, signup page 구성
    로그인, 회원가입 페이지 마크업 완료(기능x)
    로고이미지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import Shorts from './pages/shorts';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="shorts" element={<Shorts />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
     </Routes>
   );
 }
