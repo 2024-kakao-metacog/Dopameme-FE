f7fffae57ab30a64ce1088e4646a27c95765f879
comgog <102729927+comgog@users.noreply.github.com>
2025-01-15 17:34:05 +0900

    feat: login, signup page 구성
    로그인, 회원가입 페이지 마크업 완료(기능x)
    로고이미지 추가

---
+++ src/pages/login.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../assets/logo_login.svg';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center bg-dopameme-bg ">
+      <div className="flex h-full w-auto">
+        <div className="flex min-h-80 min-w-[360px] flex-col items-center justify-center">
+          <Logo />
+          <div className="flex w-[346px] flex-col pt-8">
+            <input className="mb-7 w-full border-b-2 border-white bg-dopameme-bg px-2 py-1 text-white focus:outline-none" type="text" id="userId" placeholder="아이디" autoFocus></input>
+            <input className="mb-7 w-full border-b-2 border-white bg-dopameme-bg px-2 py-1 text-white focus:outline-none" type="password" id="password" placeholder="비밀번호"></input>
+            <div className="flex w-full justify-center">
+              <Link to="/signup">
+                <button id="signupBut" className="px-10 text-white">
+                  회원가입
+                </button>
+              </Link>
+              <button id="loginBut" className="px-10 text-white">
+                로그인
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
