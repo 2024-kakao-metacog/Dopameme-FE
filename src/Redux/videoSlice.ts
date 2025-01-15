1d609244459fbda199f098e1d847189b15ffda76
SANGHYEOP <127101419+ohsanghyeop@users.noreply.github.com>
2025-01-15 23:04:31 +0900

    refactor: Video 중복타입 정리
    videoApi.ts: Video 중복타입 정리
    videoSlice.ts: Video 중복타입 정리
    Video.ts: Video type
    tsconfig.json: include ["src"] -> include ["src", "src/**/*"]

--- src/Redux/videoSlice.ts
+++ src/Redux/videoSlice.ts
@@ -1,17 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface Video {
-  videoId: string; // 비디오 ID
-  title: string; // 제목
-  videoUrl: string; // 비디오 URL
-  thumbnailUrl: string; // 썸네일 URL
-  publishedAt: number; // 게시 날짜 (Unix 타임스탬프)
-  userId: string; // 사용자 ID
-  userNickname: string; // 사용자 닉네임
-  isOwner: boolean; // 현재 사용자가 소유자인지 여부
-  isSubscribed: boolean; // 구독 여부
-  canSubscribe: boolean; // 구독 가능 여부
-}
+import { Video } from '../types/Video';
 
 interface VideoState {
   videos: Video[];
