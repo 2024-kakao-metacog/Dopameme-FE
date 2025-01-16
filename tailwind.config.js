73c2b2346a9b9750268cedba3fda57c13a32b89b
bokrae <102729927+comgog@users.noreply.github.com>
2025-01-16 17:12:53 +0900

    merge: menubar (#9)
    메인, 쇼츠 재생 페이지에서 보일 메뉴바를 구성하였습니다.
    메뉴바 스크롤기능을 위해 tailwind-scrollbar-hide 플러그인을
    설치하였습니다.
    app.tsx의 페이지 route관계 수정하였습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,9 @@ module.exports = {
       colors: {
         'kakao-yellow': '#FAE100',
         'dopameme-bg': '#0F0F0F',
+        'menubar-highlight': '#3B3B3B',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
