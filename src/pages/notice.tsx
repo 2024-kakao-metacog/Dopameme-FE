6fa8a1fe391731295834af15d103ad1d72767348
comgog <102729927+comgog@users.noreply.github.com>
2025-01-17 12:18:36 +0900

    feat: notice page 구성완료
    구독중인 채널의 영상업로드 알림 페이지 마크업 완료 및 메뉴바에 연결

--- src/pages/notice.tsx
+++ src/pages/notice.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
-import { ReactComponent as Logo } from '../assets/logo_login.svg';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="flex size-full flex-col">
       {/* header */}
-      <div className="h-32 w-full border-b-2 border-white"></div>
+      <div className="flex h-32 w-full items-center border-b-2 border-white">
+        <div className="h-auto w-full pl-14 text-2xl font-bold text-white">알림</div>
+      </div>
       {/* contents */}
-      <div className="flex-1"></div>
+      <div className="flex-1 p-6">
+        <div className="flex size-full flex-col px-4">
+          {/* notice container */}
+          <div className="mb-2 flex h-32 w-full min-w-[700px] max-w-[1000px] rounded-lg bg-menubar-highlight p-3">
+            {/* 동영상 정보  */}
+            <div className="flex size-full flex-1 flex-col px-1.5">
+              <div className="flex h-full overflow-hidden  pb-4">
+                <p className="line-clamp-2 text-2xl text-white">제목 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅁㄴㅁㄴㄻㅇㅁㄹㅊㅍㅌ퓬ㅇㅎㄹㅇㄻㄴㅇㅊㅌ</p>
+              </div>
+              <div className="flex h-auto w-full justify-between">
+                <div className="truncate font-bold text-gray-400">백종원 PAIK JONG WON</div>
+                <div className="flex">
+                  <div className="font-bold text-gray-400">4시간 전</div>
+                  <div className="ml-2 flex w-4 justify-center font-bold text-gray-400">:</div>
+                </div>
+              </div>
+            </div>
+            {/* 동영상 썸네일 */}
+            <div className="ml-2 h-full w-44 bg-slate-200"></div>
+          </div>
+          <div className="mb-2 flex h-32 w-full min-w-[700px] max-w-[1000px] rounded-lg bg-menubar-highlight p-3">
+            {/* 동영상 정보  */}
+            <div className="flex size-full flex-1 flex-col px-1.5">
+              <div className="flex h-full overflow-hidden  pb-4">
+                <p className="line-clamp-2 text-2xl text-white">제목 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅁㄴㅁㄴㄻㅇㅁㄹㅊㅍㅌ퓬ㅇㅎㄹㅇㄻㄴㅇㅊㅌ</p>
+              </div>
+              <div className="flex h-auto w-full justify-between">
+                <div className="truncate font-bold text-gray-400">백종원 PAIK JONG WON</div>
+                <div className="flex">
+                  <div className="font-bold text-gray-400">4시간 전</div>
+                  <div className="ml-2 flex w-4 justify-center font-bold text-gray-400">:</div>
+                </div>
+              </div>
+            </div>
+            {/* 동영상 썸네일 */}
+            <div className="ml-2 h-full w-44 bg-slate-200"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
