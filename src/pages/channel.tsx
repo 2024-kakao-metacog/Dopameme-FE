9e993398c474aa405625632cec9b120abffff740
comgog <102729927+comgog@users.noreply.github.com>
2025-01-20 16:48:41 +0900

    feat: logout기능 구현
    로그인한 아이디와 동일한 계정 페이지에서 로그아웃이 가능하도록 기능을
    구현함.
    outlet영역이 전체영역으로 잡힐 수 있도록 수정.

--- src/pages/channel.tsx
+++ src/pages/channel.tsx
@@ -1,53 +1,36 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { RootState } from '../Redux/store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearAuth } from '../Redux/slice/authSlice';
 
 function App() {
   const { id } = useParams<{ id: string }>();
+  const { id: myId } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(clearAuth());
+  };
 
   return (
     <div className="flex size-full flex-col">
       {/* header */}
-      <div className="flex h-32 w-full items-center border-b-2 border-white">
-        <div className="h-auto w-full pl-14 text-2xl font-bold text-white">{id}</div>
+      <div className="flex h-32 min-h-32 w-full items-center border-b-2 border-white">
+        <div className="flex h-full items-center pl-14 pr-3 text-4xl font-bold text-white">{id}</div>
+        {id === myId && (
+          <div className="flex h-full items-center">
+            <button className="border-b-2 border-dopameme-bg text-white hover:border-white" onClick={handleLogout}>
+              로그아웃
+            </button>
+          </div>
+        )}
       </div>
       {/* contents */}
       <div className="flex-1 p-6">
         <div className="flex size-full flex-col px-4">
           {/* notice container */}
-          <div className="mb-2 flex h-32 w-full min-w-[700px] max-w-[1000px] rounded-lg bg-menubar-highlight p-3">
-            {/* 동영상 정보  */}
-            <div className="flex size-full flex-1 flex-col px-1.5">
-              <div className="flex h-full overflow-hidden pb-4">
-                <p className="line-clamp-2 text-2xl text-white">제목 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅁㄴㅁㄴㄻㅇㅁㄹㅊㅍㅌ퓬ㅇㅎㄹㅇㄻㄴㅇㅊㅌ</p>
-              </div>
-              <div className="flex h-auto w-full justify-between">
-                <div className="truncate font-bold text-gray-400">백종원 PAIK JONG WON</div>
-                <div className="flex">
-                  <div className="font-bold text-gray-400">4시간 전</div>
-                  <div className="ml-2 flex w-4 justify-center font-bold text-gray-400">:</div>
-                </div>
-              </div>
-            </div>
-            {/* 동영상 썸네일 */}
-            <div className="ml-2 h-full w-44 bg-slate-200"></div>
-          </div>
-          <div className="mb-2 flex h-32 w-full min-w-[700px] max-w-[1000px] rounded-lg bg-menubar-highlight p-3">
-            {/* 동영상 정보  */}
-            <div className="flex size-full flex-1 flex-col px-1.5">
-              <div className="flex h-full overflow-hidden pb-4">
-                <p className="line-clamp-2 text-2xl text-white">제목 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅁㄴㅁㄴㄻㅇㅁㄹㅊㅍㅌ퓬ㅇㅎㄹㅇㄻㄴㅇㅊㅌ</p>
-              </div>
-              <div className="flex h-auto w-full justify-between">
-                <div className="truncate font-bold text-gray-400">백종원 PAIK JONG WON</div>
-                <div className="flex">
-                  <div className="font-bold text-gray-400">4시간 전</div>
-                  <div className="ml-2 flex w-4 justify-center font-bold text-gray-400">:</div>
-                </div>
-              </div>
-            </div>
-            {/* 동영상 썸네일 */}
-            <div className="ml-2 h-full w-44 bg-slate-200"></div>
-          </div>
+          <div className="w-full text-white">test</div>
         </div>
       </div>
     </div>
