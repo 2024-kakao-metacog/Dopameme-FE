a6f5ad09a2aa263ca34e8a03f9cee3adeb8d8ac8
comgog <102729927+comgog@users.noreply.github.com>
2025-01-20 12:22:07 +0900

    feat: 메뉴바 구독 리스트 redux 연결
    메뉴바에 표시되는 구독 목록을 redux에 저장된 데이터를 기반으로
    랜더링합니다.
    로그인한 유저의 구독 목록을 redux에 저장하기 위한 로직을 구현하였습니다.
    (로그인시에 구독정보를 서버에서 받아와 저장합니다)
    구독한 채널별 페이지를 구성하기위한 라우팅과, 채널별 페이지를 임시로
    구성하였습니다.

--- src/Redux/slice/authSlice.ts
+++ src/Redux/slice/authSlice.ts
@@ -3,24 +3,28 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface AuthState {
   token: string | null;
   nickname: string | null;
+  id: string | null;
 }
 
 const initialState: AuthState = {
   token: null,
   nickname: null,
+  id: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAuth: (state, action: PayloadAction<{ token: string; nickname: string }>) => {
+    setAuth: (state, action: PayloadAction<{ token: string; nickname: string; id: string }>) => {
       state.token = action.payload.token;
       state.nickname = action.payload.nickname;
+      state.id = action.payload.id;
     },
     clearAuth: state => {
       state.token = null;
       state.nickname = null;
+      state.id = null;
     },
   },
 });
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const { token, nickname } = useSelector((state: RootState) => state.auth);
+  const subscriptions = useSelector((state: RootState) => state.subscriptions.subscriptions); // 구독 목록 가져오기
 
   return (
     <div className="flex h-screen w-screen items-center overflow-hidden bg-dopameme-bg">
@@ -39,40 +40,20 @@ function App() {
           {/* sub list */}
           <div>
             <div className="mb-1 rounded-md p-1 text-base font-bold text-kakao-yellow">구독</div>
-
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
+            <Link key="test" to="testId">
+              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">test nickname {/* 채널 이름으로 닉네임 사용 */}</div>
             </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
-            <Link to="/main">
-              <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">선쩔티비</div>
-            </Link>
+            {subscriptions && subscriptions.length > 0 ? (
+              subscriptions.map(subscription => (
+                <Link key={subscription.id} to={`/${subscription.id}`}>
+                  <div className="mb-1 rounded-md p-1 text-base font-normal text-white hover:bg-menubar-highlight">
+                    {subscription.nickname} {/* 채널 이름으로 닉네임 사용 */}
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="mb-1 rounded-md p-1 text-sm font-normal text-white">구독한 채널이 없습니다.</div>
+            )}
           </div>
         </div>
       </div>
