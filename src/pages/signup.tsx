6e3a6c2237227adc8d5ead699992b5c006df4912
comgog <102729927+comgog@users.noreply.github.com>
2025-01-22 11:07:56 +0900

    feat: sign up 기능 구현
    회원가입기능을 구현하였습니다.
    회원가입 실패시 에러메세지에 따른 문구를 표시

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,21 +1,62 @@
 import React, { useState } from 'react';
 import { ReactComponent as Logo } from '../assets/logo_login.svg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useRegisterMutation } from '../Redux/slice/authApi';
 
 function App() {
-  const [id, setId] = useState('');
+  const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
-  const [register, { isLoading, isError }] = useRegisterMutation();
+  const [error, setError] = useState('');
+  const [register, { isLoading }] = useRegisterMutation();
+  const navigate = useNavigate();
+
+  type ApiError = {
+    data: {
+      statusCode: string | number;
+      message: string;
+    };
+  };
+
+  const isApiError = (err: unknown): err is ApiError => {
+    return (
+      typeof err === 'object' &&
+      err !== null &&
+      'data' in err &&
+      typeof (err as ApiError).data === 'object' &&
+      (typeof (err as ApiError).data.statusCode === 'string' || typeof (err as ApiError).data.statusCode === 'number') &&
+      typeof (err as ApiError).data.message === 'string'
+    );
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (!userId) {
+      setError('아이디를 입력해 주세요');
+      return;
+    } else if (!password) {
+      setError('비밀번호를 입력해 주세요');
+      return;
+    } else if (!nickname) {
+      setError('닉네임을 입력해 주세요');
+      return;
+    }
     try {
-      const response = await register({ id, password, nickname }).unwrap();
+      setError('');
+      const response = await register({ userId, password, nickname }).unwrap();
       console.log('회원가입 성공:', response.message);
+      navigate(`/login`);
     } catch (err: unknown) {
-      console.error('Login failed:', err);
+      console.error('sign up failed:', err);
+      if (isApiError(err) && err.data.statusCode == 409) {
+        if (err.data.message == 'user_id already exists') {
+          setError('이미 존재하는 아이디입니다.');
+        } else {
+          setError('이미 존재하는 닉네임입니다.');
+        }
+      } else {
+        setError('알 수 없는 오류가 발생했습니다.');
+      }
     }
   };
 
@@ -31,8 +72,8 @@ function App() {
               <input
                 className="mb-7 w-full border-b-2 border-white bg-dopameme-bg px-2 py-1 text-white focus:outline-none"
                 type="text"
-                value={id}
-                onChange={e => setId(e.target.value)}
+                value={userId}
+                onChange={e => setUserId(e.target.value)}
                 id="userId"
                 placeholder="아이디"
                 autoFocus
@@ -63,7 +104,9 @@ function App() {
               </div>
             </div>
           </form>
-          <div className="mt-5 flex h-5 w-full justify-center text-red-600">{isError && <p className="text-red-500">회원가입 실패</p>}</div>
+          <div className="mt-5 flex h-5 w-full justify-center text-red-600">
+            <p className="text-red-500">{error}</p>
+          </div>
         </div>
       </div>
     </div>
