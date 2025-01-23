584dd2cc87b68a851189ba437c8ca3b6baf8106d
comgog <102729927+comgog@users.noreply.github.com>
2025-01-23 11:52:10 +0900

    feat: login/auth
    로그인 기능을 구현하였습니다.
    로그인시 받게되는 acess토큰을 로컬스토리지에 저장.
    access토큰을 디코딩하여 id를 얻고 id로 닉네임을 얻어 로컬스토리지에 저장
    유저정보가 있을 때, 메뉴바의 로그인 버튼을 유저 페이지로 대체

--- src/Redux/slice/authApi.ts
+++ src/Redux/slice/authApi.ts
@@ -2,14 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import API_URL from '../../config/env';
 
 interface LoginRequest {
-  id: string;
+  userId: string;
   password: string;
 }
 
 interface LoginResponse {
-  token: string;
-  nickname: string;
-  id: string;
+  data: unknown;
+  headers: {
+    authorization: string;
+  };
 }
 
 interface SignupRequest {
@@ -24,14 +25,22 @@ interface SignupResponse {
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL, credentials: 'include' }),
   endpoints: builder => ({
     login: builder.mutation<LoginResponse, LoginRequest>({
       query: credentials => ({
-        url: '/login',
+        url: 'v1/auth',
         method: 'POST',
         body: credentials,
       }),
+      transformResponse: (response: LoginResponse, meta) => {
+        // 헤더 정보를 추출하여 리턴
+        const authorization = meta?.response?.headers?.get('authorization') || '';
+        return {
+          data: response,
+          headers: { authorization },
+        };
+      },
     }),
     register: builder.mutation<SignupResponse, SignupRequest>({
       query: userData => ({
---
+++ src/utils/setUserInfo.ts
@@ -0,0 +1,33 @@
+import { AppDispatch } from '../Redux/store/store';
+import axios from 'axios';
+import { setAuth } from '../Redux/slice/authSlice';
+import API_URL from '../config/env';
+import { jwtDecode } from 'jwt-decode';
+
+export const getInfoByToken = () => async (dispatch: AppDispatch) => {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (accessToken) {
+    try {
+      // 디코딩을 통해 user ID 추출
+      const decodedToken = jwtDecode<{ userId: string }>(accessToken);
+      const userId = decodedToken.userId;
+
+      // API 호출을 통해 닉네임 정보 가져오기
+      console.log(`${API_URL}v1/user?id=${userId}`);
+      const response = await axios.get(`${API_URL}v1/user?id=${userId}`);
+      const nickname = response.data.snippet.nickname || 'user';
+
+      // 액션 디스패치
+      dispatch(
+        setAuth({
+          accessToken,
+          nickname,
+          id: userId,
+        }),
+      );
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+    }
+  }
+};
