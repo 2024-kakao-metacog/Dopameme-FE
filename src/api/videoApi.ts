58668c162a3c73ad0398f9c06b337f96e748916a
SANGHYEOP <127101419+ohsanghyeop@users.noreply.github.com>
2025-01-23 16:53:18 +0900

    feat: channel-video-list
    videoApi.ts: userId로 메타데이터 가져오는 기능 추가
    channel.tsx: 채널의 userId를 이용해 비디오 메타데이터를 받아와서
    표시하는 기능 추가
    main.tsx: 비디오 카드 최대크기 축소

--- src/api/videoApi.ts
+++ src/api/videoApi.ts
@@ -5,22 +5,23 @@ import { Video } from '../types/Video';
 interface VideoApiResponse {
   snippet: Video;
   videoUrl: Video['videoUrl']; // Video의 videoUrl 속성만 재사용
+  userId: Video['userId']; // Video의 videoId 속성만 재사용
 }
 
 // 메타데이터를 파싱하는 함수
-function parseVideoMetadata(data: VideoApiResponse[]): Video[] {
-  return data.map(item => ({
-    title: item.snippet.title,
-    videoUrl: item.snippet.videoUrl,
-    thumbnailUrl: item.snippet.thumbnailUrl,
-    publishedAt: new Date(item.snippet.publishedAt).toISOString(),
-    userId: item.snippet.userId,
-    userNickname: item.snippet.userNickname,
-    isOwner: item.snippet.isOwner,
-    isSubscribed: item.snippet.isSubscribed,
-    canSubscribe: item.snippet.canSubscribe,
-  }));
-}
+// function parseVideoMetadata(data: VideoApiResponse[]): Video[] {
+//   return data.map(data => ({
+//     title: data.snippet.title,
+//     videoUrl: data.snippet.videoUrl,
+//     thumbnailUrl: data.snippet.thumbnailUrl,
+//     publishedAt: new Date(data.snippet.publishedAt).toISOString(),
+//     userId: data.snippet.userId,
+//     userNickname: data.snippet.userNickname,
+//     isOwner: data.snippet.isOwner,
+//     isSubscribed: data.snippet.isSubscribed,
+//     canSubscribe: data.snippet.canSubscribe,
+//   }));
+// }
 
 // 단일 메타데이터를 파싱하는 함수
 function parseSingleVideoMetadata(data: VideoApiResponse): Video {
@@ -59,4 +60,26 @@ async function fetchVideoMetadataByUrl(videoUrl: string): Promise<Video | null> 
   }
 }
 
-export { parseVideoMetadata, fetchVideoMetadata, fetchVideoMetadataByUrl };
+// 유저 아이디로 그 유저의 동영상 메타데이터만 가져오는 함수
+async function fetchVideoMetadataById(userId: string): Promise<Video[]> {
+  try {
+    const response = await axios.get(`${API_URL}v1/video/metadatas?userId=${userId}`);
+    const snippet = response.data.snippet;
+
+    if (!snippet) {
+      // snippet이 null 또는 undefined인 경우
+      return [];
+    } else if (Array.isArray(snippet)) {
+      // snippet이 배열인 경우
+      return snippet;
+    } else {
+      // snippet이 단일 객체인 경우
+      return [snippet];
+    }
+  } catch (error) {
+    console.error('Error fetching video metadata by user ID', error);
+    throw error;
+  }
+}
+
+export { fetchVideoMetadata, fetchVideoMetadataByUrl, fetchVideoMetadataById };
